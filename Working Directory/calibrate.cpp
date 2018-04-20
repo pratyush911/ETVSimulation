da5f4cd277084c0f3bb3fe3264d17e4081913e94
Pratyush Maini <pratyush.maini@gmail.com>
2018-04-21 02:13:45 +0530

    Algo for Local Calib completed (temp)

--- Working Directory/calibrate.cpp
+++ Working Directory/calibrate.cpp
@@ -56,3 +56,55 @@ cMatrix3d rotate2(cVector3d A,cVector3d b,cVector3d B,cVector3d normal){
 	result = dir1.cRotAxisAngleRad(normal.x(),normal.y(),normal.z(),angleradian);
 	return result;
 }
+
+//finding the normals
+
+cVector3d normal_find (cVector3d a,cVector3d b,cVector3d c) {
+cVector3d dir0( (a.(x) - b.(x)), (a.(y) - b.(y)), (a.(z) - b.(z)) );
+cVector3d dir1((c.(x) - b.(x)), (c.(y) - b.(y)), (c.(z) - b.(z)) );
+cVector3d norm;
+dir1.crossr(dir0, norm);
+
+return norm;
+
+}
+
+void calibrate3d(cVector3d a,cVector3d b,cVector3d c,cVector3d A,cVector3d B,cVector3d C){
+	double scaling_factor;
+	cMatrix3d rotate1,rotate2;
+	cVector3d translate,normal;
+	scaling_factor = scale1(a,b,A,B);
+	normal = normal_find(A,B,C);	
+
+	//applying scale to the three points
+	a = a*scaling_factor;
+	b = b*scaling_factor;
+	c = c*scaling_factor;
+
+	// rotate1 = rotate1(a,b,c,A,B,C);
+	
+	//applying the rotation 1 (aligning the normal vectors of the plane)
+	rotate1.mulr(a,a);
+	rotate1.mulr(b,b);
+	rotate1.mulr(c,c);
+	
+	translate = translate1(a,A);
+	
+	//applying the translation (making one point coincide with the physical skull's point)
+	a.add(translate);
+	b.add(translate);
+	c.add(translate);	
+
+	rotate2 = rotate2(a,b,B,normal);
+	
+
+	//Applying changes to the mesh
+	m_meshVentricles->scaleXYZ(scaling_factor, scaling_factor, scaling_factor);
+	m_meshThirdVentricles->scaleXYZ(scaling_factor, scaling_factor, scaling_factor);
+	m_meshThalastriateVein->scaleXYZ(scaling_factor, scaling_factor, scaling_factor);
+	m_meshChoroidPlexus->scaleXYZ(scaling_factor, scaling_factor, scaling_factor);
+
+
+
+}
+
